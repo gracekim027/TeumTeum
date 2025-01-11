0140a086201a6ee9c719d8fd7bbdcd463e304f85
Grace Kim 김은혜 <gracekim027@gmail.com>
2025-01-11 11:58:48 +0900

    create project structure

---
+++ frontend/piyakton/piyakton/piyaktonApp.swift
@@ -0,0 +1,17 @@
+//
+//  piyaktonApp.swift
+//  piyakton
+//
+//  Created by Grace Kim on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct piyaktonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ frontend/piyakton/piyaktonTests/piyaktonTests.swift
@@ -0,0 +1,17 @@
+//
+//  piyaktonTests.swift
+//  piyaktonTests
+//
+//  Created by Grace Kim on 1/11/25.
+//
+
+import Testing
+@testable import piyakton
+
+struct piyaktonTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
